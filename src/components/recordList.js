eff5d4e38311c232843f10b40ca3db22d127f4e6
m.simardavid@gmail.com <m.simardavid@gmail.com>
2023-11-04 13:42:50 +0900

    can display data from mongoSB

--- src/components/recordList.js
+++ src/components/recordList.js
@@ -1,83 +1,87 @@
-
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 const Record = (props) => (
- <tr>
-   <td>{props.record.name}</td>
-   <td>{props.record.position}</td>
-   <td>{props.record.level}</td>
-   <td>
-     <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Edit</Link> |
-     <button className="btn btn-link"
-       onClick={() => {
-         props.deleteRecord(props.record._id);
-       }}
-     >
-       Delete
-     </button>
-   </td>
- </tr>
+  <tr>
+    <td>{props.record.name}</td>
+    <td>{props.record.position}</td>
+    <td>{props.record.level}</td>
+    <td>
+      <Link className="btn btn-link" to={`/edit/${props.record._id}`}>
+        Edit
+      </Link>{" "}
+      |
+      <button
+        className="btn btn-link"
+        onClick={() => {
+          props.deleteRecord(props.record._id);
+        }}
+      >
+        Delete
+      </button>
+    </td>
+  </tr>
 );
 export default function RecordList() {
- const [records, setRecords] = useState([]);
-  // This method fetches the records from the database.
- useEffect(() => {
-   async function getRecords() {
+  const [records, setRecords] = useState([]);
+
+  const getRecords = async function(){
+    try{
     console.log("   async function getRecords()");
-    let response
-    try {
+    let response;
       response = await fetch(`http://localhost:5000/record/`);
-      if (!response.ok) {
-       const message = `An error occurred: ${response.statusText}`;
-       window.alert(message);
-       return;
-      }
-    } catch (error) {
-      window.alert(error);
-      
-    }
+      // if (!response.ok) {
+      //  const message = `An error occurred: ${response.statusText}`;
+      //  window.alert(message);
+      //  return;
+      // }
       const records = await response.json();
       console.log(records);
-     setRecords(records);
-   }
+      let recordsArray = []
+      recordsArray.push(records)
+      setRecords(recordsArray);
+  } catch (error) {
+    window.alert(error);
+    console.log("error:", error);
+  }
+  }
+  // This method fetches the records from the database.
+  useEffect(() => {
+      
     getRecords();
     return;
- }, [records.length]);
+  }, []);
+  // }, [records.length]);
   // This method will delete a record
- async function deleteRecord(id) {
-   await fetch(`http://localhost:5000/${id}`, {
-     method: "DELETE"
-   });
+  async function deleteRecord(id) {
+    await fetch(`http://localhost:5000/${id}`, {
+      method: "DELETE",
+    });
     const newRecords = records.filter((el) => el._id !== id);
-   setRecords(newRecords);
- }
+    setRecords(newRecords);
+  }
   // This method will map out the records on the table
- function recordList() {
-   return records.map((record) => {
-     return (
-       <Record
-         record={record}
-         deleteRecord={() => deleteRecord(record._id)}
-         key={record._id}
-       />
-     );
-   });
- }
+  function recordList() {
+    return records.map((record) => {
+      return (
+        <Record record={record} deleteRecord={() => deleteRecord(record._id)} key={record._id} />
+      );
+    });
+  }
   // This following section will display the table with the records of individuals.
- return (
-   <div>
-     <h3>Record List</h3>
-     <table className="table table-striped" style={{ marginTop: 20 }}>
-       <thead>
-         <tr>
-           <th>Name</th>
-           <th>Position</th>
-           <th>Level</th>
-           <th>Action</th>
-         </tr>
-       </thead>
-       <tbody>{recordList()}</tbody>
-     </table>
-   </div>
- );
+  return (
+    <div>
+      <h3>Record List</h3>
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Level</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{recordList()}</tbody>
+      </table>
+    </div>
+  );
 }
